6e2f1ea206c118ac93c341879d3f1a3a7a0e86cd
lewfer <llewelyn@arcwave.co.uk>
2022-09-29 11:24:25 +0100

    Fixing comms, tidy up

--- house.py
+++ house.py
@@ -7,22 +7,6 @@ Also provides a UI to show what's happening in the house.
 Reads available energy from the national grid and turns on whatever devices can be powered.
 """
 
-# Setup
-###################################################################################################
-
-
-# Define the devices we need to power
-# These will be populated by the microbit
-devices = {}
-"""
-devices = [{"name":"light", "power_needed":100, "on":1, "powered":0}, 
-           {"name":"toaster","power_needed":100, "on":1, "powered":0}, 
-           {"name":"car","power_needed":100, "on":1, "powered":0}, 
-           {"name":"tv","power_needed":100, "on":1, "powered":0}, 
-           {"name":"dishwasher","power_needed":100, "on":1, "powered":0}]
-"""
-
-
 # Imports
 ###################################################################################################
 
@@ -37,26 +21,54 @@ from utils import *
 from settings import *
 from threading import Thread
 
+# Setup
+###################################################################################################
+
 # Run until the user asks to quit
 running = True
 
 # Set to true when microbit is ready
 ready = False
 
-# Main
-###################################################################################################
 
 # Pygame window parameters
 windowWidth = 500
-windowHeight = 500
+windowHeight = 600
 maxBar = windowWidth-20
 mousex = 0
 mousey = 0
+barheight = 30
+bargap = 2
 
-# Object to connect to microbit
+# Pygame colors
+DEVICE_ON = green
+DEVICE_OFF = grey
+DEVICE_NOPOWER = pygame.Color("cadetblue1")
+GAUGE_GOOD = green
+GAUGE_DANGER = yellow
+GAUGE_BAD = red
+GAUGE_NODEMAND = grey
+GAUGE_BAR = white
+
+# Object to connect to microbit via serial
 microbit = Microbit(house_serialport)
 microbit.connect()
 
+# Define the devices we need to power
+# These will be populated by the microbit
+devices = {}
+
+# Data logging
+dataNeeded = []
+dataUsed = []
+
+# Parameters to adjust
+refreshPeriod = 1000    # how often to consume energy (milliseconds).  determines screen refresh rate
+consumptionRate = 1/5   # proportion of device power used on each refresh period
+
+
+# Functions
+###################################################################################################
 
 # Function to listen for serial messages from the microbit
 # This will run on a separate thread to the pygame loop
@@ -69,26 +81,20 @@ def listen():
             print("new message",message)
 
             if message=="clear":
-                print("Receive:","clear")
+                # Clear all devices
+                ready = False
                 devices.clear()
+                #sleep(5)
 
-            elif message.startswith("debug"):
-                print(message)
-
-            elif message=="get":
-                for device in devices.values():
-                    if device["on"] and device["powered"]:
-                        message = f"{device['name']}=1\n"
-                    else:
-                        message = f"{device['name']}=0\n"
-                    #message += "\n"
-                    microbit.write(message)
-                    print("Sending:",message)     
-
-            elif message=="ready":           
+            elif message=="ready":
                 ready = True
 
+            elif message.startswith("debug"):
+                # Debug message from microbit
+                print(message)
+
             elif message.startswith("get"):
+                # Get on/off status for device
                 split = message.split("=")
                 device = devices[split[1]]
                 if device["on"] and device["powered"]:
@@ -96,134 +102,150 @@ def listen():
                 else:
                     status = "0\n"
                 microbit.write(status)
-                print("Sending:",status)   
+                #print("Sending:",status)   
 
             elif (len(message)>0):
-                print("Receive:",message)
+                # Adding a new device
+                #print("Receive:",message)
                 split = message.split("=")
                 devices[split[0]] = {"name":split[0], "power_needed":int(split[1]), "on":0, "powered":0}
         except Exception as e:
             print(e)
             pass
 
+def getTotalEnergy():
+    # Find out how much energy is available at the national grid
+    url = "http://lewfer.pythonanywhere.com/gettotalenergy"
+    response = requests.get(url)
+    return int(response.json())
 
-# Create a new thread for the listen function
+def useEnergy(units):
+    # Tell National Grid how much we used
+    print(totalEnergyUsed)
+    url = f"http://lewfer.pythonanywhere.com/use?energy={units}"
+    response = requests.get(url)
+    #print(response.json())
+
+def getEnergyConsumption(totalEnergyAvailable):
+    # Power each device (turn on "powered") until we run out of energy
+    maxEnergyNeeded = 0     
+    totalEnergyNeeded = 0  
+    totalEnergyUsed = 0     
+    for device in devices.values():
+        devicePowerNeeded = device["power_needed"]
+        maxEnergyNeeded += devicePowerNeeded
+        if device["on"]:
+            totalEnergyNeeded += devicePowerNeeded #*consumptionRate
+            if devicePowerNeeded <= totalEnergyAvailable:
+                device["powered"] = 1
+                totalEnergyUsed += devicePowerNeeded #*consumptionRate
+                totalEnergyAvailable -= devicePowerNeeded #*consumptionRate
+            else:
+                device["powered"] = 0    
+    return maxEnergyNeeded,totalEnergyNeeded,totalEnergyUsed
+
+
+# Main
+###################################################################################################
+
+# Create a new thread to listen from messages from the microbit
 Thread(target=listen).start()
 
 
 # Startup pygame window
 pygame.init()
 screen = pygame.display.set_mode([windowWidth, windowHeight])
+pygame.display.set_caption('House Monitor')
+icon = pygame.image.load('house.png')
+pygame.display.set_icon(icon)
 
-DRAWENERGY= pygame.USEREVENT + 1 
-pygame.time.set_timer(DRAWENERGY, 5000)
+# Create objects on the screen
+graph = Graph(100,100+(barheight+bargap)*5+50,300,200, screen)
 
-totalEnergyNeeded = 0
-totalEnergyAvailable = 0
+# Set a timer for how often to consume energy
+CONSUMEENERGY= pygame.USEREVENT + 1 
+pygame.time.set_timer(CONSUMEENERGY, refreshPeriod)
 
+# Energy usage variables
+maxEnergyNeeded = 0             # if all devices on
+totalEnergyNeeded = 0           # for currently on devices
+totalEnergyAvailable = 0        # what the powerstation can give
+totalEnergyUsed = 0             # what was actually used
 
 
 # Main Pygame loop
 while running:
 
-    # Did the user click the window close button?
+    # Check pygame events
     mousex = -1
     mousey = -1
-    drawEnergy = False 
+    consumeEnergy = False 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             mousex, mousey = pos
-        if event.type == DRAWENERGY:
-            drawEnergy = True
+        if event.type == CONSUMEENERGY:
+            consumeEnergy = True
 
     # Fill the background with white
     screen.fill((255, 255, 255))       
 
-    # Work out total energy needed by the devices
-    totalEnergyNeeded = 0
-    for device in devices.values():
-        devicePowerNeeded = device["power_needed"]
-        totalEnergyNeeded += devicePowerNeeded
-    #print("totalEnergyNeeded",totalEnergyNeeded)
-
-
     if ready:
-        if drawEnergy:
-
+        if consumeEnergy:
 
             # Find out how much energy is available at the national grid
-            url = "http://lewfer.pythonanywhere.com/gettotalenergy"
-            response = requests.get(url)
-            totalEnergyAvailable = int(response.json())
-            #print("Available:", totalEnergyAvailable)
+            totalEnergyAvailable = getTotalEnergy()
 
-            # Power each device (turn on "powered") until we run out of energy
-            totalEnergyUsed = 0
-            for device in devices.values():
-                if device["on"]:
-                    devicePowerNeeded = device["power_needed"]
-                    if devicePowerNeeded <= totalEnergyAvailable:
-                        device["powered"] = 1
-                        totalEnergyUsed += devicePowerNeeded
-                        totalEnergyAvailable -= devicePowerNeeded
-                    else:
-                        device["powered"] = 0
-            #print(f"Total energy used {totalEnergyUsed}")
+            # Power each device (turn on "powered") until we reach the availability limit
+            maxEnergyNeeded,totalEnergyNeeded,totalEnergyUsed = getEnergyConsumption(totalEnergyAvailable)
 
             # Tell National Grid how much we used
-            url = f"http://lewfer.pythonanywhere.com/use?energy={totalEnergyUsed}"
-            response = requests.get(url)
-            #print(response.json())
-
-            # Tell microbit which devices to turn on or off
-            #message = ""
-            """
-            for device in devices:
-                if device["on"] and device["powered"]:
-                    message = f"{device['name']}=1\n"
-                else:
-                    message = f"{device['name']}=0\n"
-                #message += "\n"
-                microbit.write(message)
-                print("Sending:",message)
-            """
-
-            #print(devices)
-
-            #print("write")
-            #totalEnergy = random.randrange(0,200)
-            #print(totalEnergy)
-            #microbit.write(f"{totalEnergy}\n")
+            useEnergy(int(totalEnergyUsed*consumptionRate))
+            
+            # Log the data
+            dataNeeded.append(totalEnergyNeeded) #/consumptionRate)
+            dataNeeded = dataNeeded[-144:]    
+            dataUsed.append(totalEnergyUsed) #/consumptionRate)
+            dataUsed = dataUsed[-144:]                
 
 
         # Draw bar to show avail energy vs needed energy
-        bar = maxBar if totalEnergyNeeded==0 else map(min(totalEnergyNeeded*2,totalEnergyAvailable), 0, totalEnergyNeeded*2, 0, maxBar)
+        # maxBar represents 2xmaxEnergyNeeded, so 1/2 maxbar, represented by the black box, is the maxEnergyNeeded
+        bar = maxBar if totalEnergyNeeded==0 else map(min(maxEnergyNeeded*2,totalEnergyAvailable), 0, maxEnergyNeeded*2, 0, maxBar)
+        neededBar = 0 if totalEnergyNeeded==0 else map(totalEnergyNeeded, 0, maxEnergyNeeded, 0, maxBar/2)
         color = red if totalEnergyAvailable<totalEnergyNeeded else yellow if totalEnergyAvailable<totalEnergyNeeded*1.5 else green
-        pygame.draw.rect(screen, red, (10,10,maxBar/2,50))    
-        pygame.draw.rect(screen, yellow, (10+maxBar/2,10,maxBar/4,50))    
-        pygame.draw.rect(screen, green, (10,10,bar,50))  
-        #pygame.draw.rect(screen, color, (10,10,bar,50))    
-        pygame.draw.rect(screen, black, (10,10,maxBar/2,50), 2)
-        textLeft(screen, 10, 10, "Avail:"+str(totalEnergyAvailable))
-        textLeft(screen, 10, 30, "Needed:"+str(totalEnergyNeeded))
+        pygame.draw.rect(screen, GAUGE_BAD, (10,10,maxBar/2,50))    
+        pygame.draw.rect(screen, GAUGE_DANGER, (10+maxBar/2,10,maxBar/4,50))    
+        pygame.draw.rect(screen, GAUGE_GOOD, (10+maxBar*3/4,10,maxBar/4,50))    
+        pygame.draw.rect(screen, GAUGE_BAR , (10,20,bar,30))   
+        if totalEnergyNeeded==0:
+            pygame.draw.rect(screen, GAUGE_NODEMAND, (10,20,bar,30))  
+        pygame.draw.rect(screen, black, (10,20,neededBar,30), 2)
+        textLeft(screen, 10, 60, "Needed:"+str(totalEnergyNeeded) + " Avail:"+str(totalEnergyAvailable), 16)
+
+        #print(maxEnergyNeeded, totalEnergyNeeded, totalEnergyAvailable,totalEnergyUsed)
+
+        # Plot graph of most recent values
+        if maxEnergyNeeded>0:
+            graph.axes(0,143,0,maxEnergyNeeded)
+            graph.plot(dataNeeded, blue)
+            graph.plot(dataUsed, red)        
 
         # Draw devices indicators
-        barheight = 30
-        bargap = 2
         for i,device in enumerate(devices.values()):
-            color = grey if not device["on"] else green if device["on"] and device["powered"] else pygame.Color("cadetblue1")
+            color = DEVICE_OFF if not device["on"] else DEVICE_ON if device["on"] and device["powered"] else DEVICE_NOPOWER
             pygame.draw.rect(screen, color, (10,100+(barheight+bargap)*i,maxBar,barheight))
-            textLeft(screen, 10, 100+(barheight+bargap)*i,device["name"])
+            textLeft(screen, 10, 100+(barheight+bargap)*i,device["name"]+" "+str(device["power_needed"]))
 
+        # Turn device on/off if clicked
         clicked = (mousey-100)//(barheight+bargap)
-        #print("Clicked",clicked)
         if clicked>=0 and clicked<=len(devices):
             devices[list(devices.keys())[clicked]]["on"] = 1-devices[list(devices.keys())[clicked]]["on"]
 
-        
+    else:
+        textLeft(screen, 10, 10, "Press RESET on your Microbit")
 
     # Update the display (flip the in-memory buffer on to the screen)
     #print("flip")
